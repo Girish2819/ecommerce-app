118e6f675c61e3d649946bec9c0953f97844c286
Girish Ranjan <girishranjan71@gmail.com>
2025-06-19 23:47:29 +0530

    .....

--- client/src/pages/Admin/AdminDashboard.js
+++ client/src/pages/Admin/AdminDashboard.js
@@ -5,7 +5,13 @@ const AdminDashboard = () => {
   const [auth] = useAuth();
   return (
     <Layout>
-          
+          <div className="col-md-9">
+            <div className="card w-75 p-3">
+              <h3> Admin Name : {auth?.user?.name}</h3>
+              <h3> Admin Email : {auth?.user?.email}</h3>
+              <h3> Admin Contact : {auth?.user?.phone}</h3>
+            </div>
+          </div>
     </Layout>
   );
 };
