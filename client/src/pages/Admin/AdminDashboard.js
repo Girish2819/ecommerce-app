f22cf81eab266e360f09b5b10245f3b2f7e79c8f
Girish Ranjan <girishranjan71@gmail.com>
2025-06-22 22:29:52 +0530

    admin dashboard updateda

--- client/src/pages/Admin/AdminDashboard.js
+++ client/src/pages/Admin/AdminDashboard.js
@@ -9,15 +9,20 @@ const AdminDashboard = () => {
   console.log("Admin Dashboard auth:", auth);
   return (
     <Layout>
-      <div className="container-fluid">
-        <div className="col-md-3">
+      <div className="container-fluid m-3 p-3">
+        <div className="row">
+          <div className="col-md-3">
+            <AdminMenu/>
+          </div>
           <div className="col-md-9">
-           <AdminMenu/>
+            <div className ="card w-75 p-3">
+              <h3>Admin Name:{auth?.user?.name}</h3>
+              <h3>Admin Email:{auth?.user?.email}</h3>
+              <h3>Admin Contact:{auth?.user?.phone}</h3>
             </div>
-            <div className="col-md-9">content</div>
           </div>
-          </div>
-         
+        </div>
+      </div>
     </Layout>
   );
 };
