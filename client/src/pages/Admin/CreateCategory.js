aafd539b674efe8c02ae2b7ac080a5212c5e48ec
Girish Ranjan <girishranjan71@gmail.com>
2025-07-03 20:44:58 +0530

    create category page is not work properly and there is another issue that when loggin with admin after reffreshing page it renders to login page

--- client/src/pages/Admin/CreateCategory.js
+++ client/src/pages/Admin/CreateCategory.js
@@ -1,8 +1,48 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react'
 import Layout from '../../components/layout/Layout'
 import AdminMenu from '../../components/layout/AdminMenu'
+import toast from 'react-hot-toast';
+import axios from 'axios';
+import CategoryForm from '../../components/form/CategoryForm';
+
 
 const CreateCategory = () => {
+  const[categories, setCategories] = useState([]);
+  const[name,setName]=useState("");
+  // handle form
+  const handleSubmit=async(e)=>{
+    e.preventDefault()
+    try{
+      const {data}=await axios.post('/api/v1/category/create-category', {name})
+      if(data?.success){
+        toast.success(`${name}is created`)
+        getAllCategories();
+      }else{
+        toast.error(data.messaage);
+      }
+    }catch(error){
+      console.log(error)
+      toast.error("somthing went wrong in input form")
+
+    }
+  }
+
+
+  // get all categories 
+  const getAllCategories = async () => {
+    try {
+      const {data} = await axios.get('/api/v1/category/get-category');
+      if (data.success) {
+        setCategories(data.categories);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong while fetching categories");
+    }
+  };
+  useEffect(() => {
+    getAllCategories();
+  }, []);
   return (
     <Layout Title={"Dashboard - Create Category"}>
         <div className="container-fluid m-3 p-3">
@@ -11,7 +51,35 @@ const CreateCategory = () => {
                 <AdminMenu/>
             </div>
             <div className="col-md-9">
-                 <h1>Create Category</h1>
+                 <h1>manage Category</h1>
+                <div className='p-3 w-50'>
+                  <CategoryForm
+
+                  handleSubmit={handleSubmit}
+                  value={name}
+                  setValue={setName}
+                  />
+                </div>
+                 <div className='w-75'>
+                  <table className="table">
+              <thead>
+               <tr>
+                 <th scope="col">Name</th>
+                  <th scope="col">Actions</th>
+                </tr>
+              </thead>
+              <tbody>
+  
+                {categories?.map((c) => (
+                  <tr key={c._id}>
+                    <td>{c.name}</td>
+                    <td><button className="btn btn-primary">edit</button></td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+
+                 </div>
             </div>
         </div>   
         </div>
