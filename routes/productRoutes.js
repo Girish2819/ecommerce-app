61f17868dc1691a54f7fafcbdcd7e2d451d11ee1
GIRISH RANJAN <166251518+Girish2819@users.noreply.github.com>
2026-03-13 13:08:54 +0530

    home page created and working fine

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,39 +1,56 @@
-import express from 'express';
-import { isAdmin, requireSignIn } from '../middlewares/authMiddleware.js';
-import { createProductController, deleteProductController, getProductController, getSingleProductController, productPhotoController, updateProductController } from '../controllers/productController.js';
-import ExpressFormidable from 'express-formidable';
-
+import express from "express";
+import {
+  createProductController,
+  deleteProductController,
+  getProductController,
+  getSingleProductController,
+  productCountController,
+  productFiltersController,
+  productListController,
+  productPhotoController,
+  updateProductController,
+} from "../controllers/productController.js";
+import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
+import formidable from "express-formidable";
 
 const router = express.Router();
 
-//routes 
+//routes
 router.post(
-    "/create-product",
-    ExpressFormidable(),
-    requireSignIn,
-    isAdmin,
-    createProductController
+  "/create-product",
+  requireSignIn,
+  isAdmin,
+  formidable(),
+  createProductController
 );
-
+//routes
 router.put(
-    "/update-product/:pid",
-    ExpressFormidable(),
-    requireSignIn,
-    isAdmin,
-    updateProductController
+  "/update-product/:pid",
+  requireSignIn,
+  isAdmin,
+  formidable(),
+  updateProductController
 );
 
-// get products 
-router.get('/get-product',getProductController); 
+//get products
+router.get("/get-product", getProductController);
 
-// get single product
-router.get('/get-product/:slug',getSingleProductController); 
+//single product
+router.get("/get-product/:slug", getSingleProductController);
 
-//get photo 
-router.get('/product-photo/:pid', productPhotoController);
+//get photo
+router.get("/product-photo/:pid", productPhotoController);
 
-// delete product
-router.delete('/delete-product/:pid', deleteProductController);
+//delete rproduct
+router.delete("/delete-product/:pid", deleteProductController);
 
-    export default router;
+//filter product
+router.post("/product-filters", productFiltersController);
 
+//product count
+router.get("/product-count", productCountController);
+
+//product per page
+router.get("/product-list/:page", productListController);
+
+export default router;
