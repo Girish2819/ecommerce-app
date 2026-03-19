17237102b1a40fc381faaf03cb915e49aa8a21aa
GIRISH RANJAN <166251518+Girish2819@users.noreply.github.com>
2026-03-20 03:05:20 +0530

    updated

--- server.js
+++ server.js
@@ -1,40 +1,46 @@
 import express from "express";
 import colors from "colors";
 import dotenv from "dotenv";
-import morgan from 'morgan'
+import morgan from "morgan";
 import connectDB from "./config/db.js";
-import authRoutes from "./routes/authRoutes.js"
+import authRoutes from "./routes/authRoutes.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
-import cors from 'cors';
+import cors from "cors";
 
-//config dotenv
+// config dotenv
 dotenv.config();
 
-// databse config
+// database config
 connectDB();
-//rest object
+
+// rest object
 const app = express();
 
-// middleware 
-app.use(cors())
-app.use (express.json())
-app.use(morgan('dev'))
+// middleware
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 
-//routes
+app.use(express.json());
+app.use(morgan("dev"));
+
+// routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 
-// rest api
-app.get("/",(req,res)=>{
-    res.send("<h1>Welcome to e-commerce app</h1>");
+// test route
+app.get("/", (req, res) => {
+  res.send("<h1>Welcome to e-commerce app</h1>");
 });
 
-//port 
+// port
 const PORT = process.env.PORT || 8080;
 
-//run listen 
-app.listen(PORT,()=>{
-    console.log(`Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan.white);
-}); 
+// run server
+app.listen(PORT, () => {
+  console.log(`Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan.white);
+});
